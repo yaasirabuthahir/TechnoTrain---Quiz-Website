94fba377d846eaff404939ed7305a5e1f7888c11
Mohammed Yaasir.A <yaasirabuthahir@gmail.com>
2026-03-23 23:11:38 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for ?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What does CSS stands for ?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Coding Style Sheet"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What does PHP stands for ?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Pogramming Hypertext Preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What does SQL stands for ?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Specified Query Logic",
+            "D. Structured Query Language"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does XML stands for ?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Media Language",
+            "D. Extensible Markup Language"
+        ]
+    }
+];
